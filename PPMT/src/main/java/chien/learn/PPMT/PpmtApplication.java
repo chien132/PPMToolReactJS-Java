3c551fd661ed6dbb6178fbe93c89a00ba0e80787
Chien Dang <44261143+chien132@users.noreply.github.com>
2021-07-09 21:34:29 +0700

    first commit

---
+++ PPMT/src/main/java/chien/learn/PPMT/PpmtApplication.java
@@ -0,0 +1,13 @@
+package chien.learn.PPMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtApplication.class, args);
+	}
+
+}
---
+++ PPMT/src/test/java/chien/learn/PPMT/PpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package chien.learn.PPMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
